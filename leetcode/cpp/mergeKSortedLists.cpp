34fdb2a42dcf2ce1c0b0c5c7b4976a26a4543fcd
Nader El-Ghotmi <43392069+NaderEG@users.noreply.github.com>
2025-07-28 14:01:40 -0400

    Create mergeKSortedLists.cpp

---
+++ leetcode/cpp/mergeKSortedLists.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) return list2;
+        if (!list2) return list1;
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+        ListNode head;
+        ListNode* tail = &head;
+
+        while(p1 && p2) {
+            if (p1->val <= p2->val) {
+                tail->next = p1;
+                p1 = p1->next;
+            } else {
+                tail->next = p2;
+                p2 = p2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = p1? p1 : p2;
+        return head.next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) {
+            return nullptr;
+        }
+        while(lists.size() > 1) {
+            vector<ListNode*> mergedLists;
+            for(int i = 0; i < lists.size(); i+=2) {
+                ListNode* l1 = lists[i];
+                ListNode* l2 = (i+1) < lists.size() ? lists[i+1] : nullptr;
+                mergedLists.push_back(mergeTwoLists(l1, l2));
+            }
+            lists = mergedLists;
+
+        }
+        return lists[0];
+        
+    }
+};
