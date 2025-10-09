1ec956cab4bf4e07d80b9f04c872ed30b0dd495a
Nader El-Ghotmi <43392069+NaderEG@users.noreply.github.com>
2025-10-09 07:47:28 -0400

    Create runningSumOf1DArray.cpp

---
+++ leetcode/cpp/runningSumOf1DArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int x = 0;
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++) {
+            x += nums[i];
+            res.push_back(x);
+        }
+        return res;
+    }
+};
